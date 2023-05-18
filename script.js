5116ef305780719d3d9399f25682c79844a7a81f
KashanAdnan <khanzaidaboy@gmail.com>
2023-05-18 11:29:41 +0500

    some-code

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
 function getResult() {
   var RandomNumber = Math.floor(Math.random() * 5);
-
-  if (RandomNumber === 0) {
-    document.getElementById(
-      "result"
-    ).innerHTML = `<img src="./images/dice1.png" />`;
-  }
-  if (RandomNumber === 1) {
-    document.getElementById(
-      "result"
-    ).innerHTML = `<img src="./images/download (1).png" />`;
-  }
-  if (RandomNumber === 2) {
-    document.getElementById(
-      "result"
-    ).innerHTML = `<img src="./images/download (2).png" />`;
-  }
-  if (RandomNumber === 3) {
-    document.getElementById(
-      "result"
-    ).innerHTML = `<img src="./images/download.png" />`;
-  }
-  if (RandomNumber === 4) {
-    document.getElementById(
-      "result"
-    ).innerHTML = `<img src="./images/images (1).png" />`;
-  }
-  if (RandomNumber === 5) {
-    document.getElementById(
-      "result"
-    ).innerHTML = `<img src="./images/images.png" />`;
-  }
+  let arr = [
+    "dice1.png",
+    "images.png",
+    "download.png",
+    "download (1).png",
+    "download (2).png",
+    "images (1).png",
+  ];
+  var number = document.getElementById("number");
+  var image = document.getElementById("result");
+  image.src = `./images/${arr[RandomNumber]}`;
+  number.innerHTML = `You Have ${RandomNumber + 1}`;
 }
